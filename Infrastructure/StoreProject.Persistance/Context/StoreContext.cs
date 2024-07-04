a3d0e42b7ac32090493d54ffd14e6b296b645df1
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-07-04 21:55:05 +0300

    Add project files.

---
+++ Infrastructure/StoreProject.Persistance/Context/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreProject.Persistance.Context
+{
+    public class StoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EGD\\SQLEXPRESS;initial Catalog=DbStoreProject; integrated security=true");
+        }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<Product> Products { get; set; }
+    }
+}
